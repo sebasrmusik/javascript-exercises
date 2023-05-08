beaf817f8505c2cd2597612a6391d0584d7a4d72
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-08 17:51:57 -0500

    Second javascript exercise finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    if(numOfTimes < 0) return 'ERROR';
+    
+    let stringFinal = '';
+    for(let i = 0; i < numOfTimes; i++){
+        stringFinal += string;
+    }
+    return stringFinal;
 };
 
 // Do not edit below this line
