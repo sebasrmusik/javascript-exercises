c7d16d39778afa9e081528857301fcd63a34cb75
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-08 18:55:15 -0500

    Fifth javascript exercise finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(a < 1 || b < 1) return 'ERROR';
+    if(typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if(a > b) {
+        let tempNum = a;
+        a = b;
+        b = tempNum;
+    }
+    for(let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
