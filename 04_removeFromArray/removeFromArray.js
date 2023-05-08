bede3a67bf51bedd3909e6bd5091d08d82b0d77a
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-08 18:47:40 -0500

    Fourth javascript exercise finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let index;
+    for(let element of elements){
+        index = array.indexOf(element);
+        if(index !== -1) array.splice(index, 1);
+    }
 
+    return array;
 };
 
 // Do not edit below this line
