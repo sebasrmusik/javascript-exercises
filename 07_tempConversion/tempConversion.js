bedbb02728856f18979d7484a12cd2ee3d932580
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-08 19:51:22 -0500

    tempConversion exercise finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenTemp) {
+  let celsiusResult = (farenTemp - 32) * (5 / 9);
+  celsiusResult = Math.round(celsiusResult * 10) / 10;
+  return celsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let farenResult = (celsiusTemp * (9 / 5)) + 32;
+  farenResult = Math.round(farenResult * 10) / 10;
+  return farenResult;
 };
 
 // Do not edit below this line
