57fed50e5b596e223545b5e266738ddb194e00b5
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-16 20:26:06 -0500

    Palindrome exercise finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // const reversed= string.split('').reverse().join('');
+    // console.log(reversed);
+    let letters = string.toLowerCase().split('');
 
+    let filteredLetters = letters.filter((element) => {
+        return element.toLowerCase() != element.toUpperCase();
+    })
+    let reversed = filteredLetters.reverse();
+    reversed = reversed.join('');
+    filteredLetters = filteredLetters.reverse().join('');
+    
+    return reversed === filteredLetters ? true : false;
 };
 
 // Do not edit below this line
