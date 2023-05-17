89d9135da16070f3f641115c1ddf6168432839ef
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-16 22:02:41 -0500

    Fibonacci exercise finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 1) return 'OOPS';
+    let number1 = 0, number2 = 1;
+    for(let i = 1; i < number; i++){
+        if(number1 < number2) number1+=number2
+        else number2 += number1;
+    }
+    if(number1 < number2) return number2
+    else return number1;
 };
 
 // Do not edit below this line
