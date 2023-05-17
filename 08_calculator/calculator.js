71fcb21b1b6f568a573c836d56a32f8e318a2ab0
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-05-16 19:42:58 -0500

    Calculator exercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let num of arr){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  for(let num of arr){
+    mul *= num;
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a < 2) return 1;
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
